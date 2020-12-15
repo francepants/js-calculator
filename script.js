f18a115e285efde091c9a0769bc97d93a4288252
Franceline Casanas <Franceline90@gmail.com>
2020-12-15 10:02:56 -0600

    added some comments and updated clear function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+//1
 // so we dont get style and classes mixed up with the JS, we'll use data attributes instead of classes for the numbers and symbols
-
 // grab all number buttons // data attr must be inside of brackets
 const numberButtons = document.querySelectorAll('[data-number]')
 
@@ -13,29 +13,47 @@ const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
+//6
+// create a calculator
 
+//2
 class Calculator {
+    //3
     // constructor: takes all inputs for it, as well as all functions for calculator
     constructor(previousOperandTextElement, currentOperandTextElement) {
         //these give us a way to set text elements inside of our class
         this.previousOperandTextElement = previousOperandTextElement
         this.currentOperandTextElement = currentOperandTextElement
+
+        //5
+        this.clear() //call the function // clear all inputs and set them to default every time we create a calculator
     }
 
+    //4
     // define functions that our calc will perform
     clear(){
-
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined //since there are no operations since it was cleared
     }
 
     delete() {
 
     }
 
-    appendNumber() {
+    appendNumber(number) {
  
     }
 
-    chooseOperation() {
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    updateDisplay() {
 
     }
 }
