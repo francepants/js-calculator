3dafe21e53b8c18cddb4b713f2b40bf68df5c49c
Franceline Casanas <Franceline90@gmail.com>
2020-12-14 20:43:25 -0600

    added button information

--- script.js
+++ script.js
@@ -3,4 +3,39 @@
 // grab all number buttons // data attr must be inside of brackets
 const numberButtons = document.querySelectorAll('[data-number]')
 
+// grab all operations
 const operationButtons = document.querySelectorAll('[data-operation]')
+
+// since there's only one equals, we use query selecter (without the all) to grab the single element
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+
+class Calculator {
+    // constructor: takes all inputs for it, as well as all functions for calculator
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        //these give us a way to set text elements inside of our class
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+    }
+
+    // define functions that our calc will perform
+    clear(){
+
+    }
+
+    delete() {
+
+    }
+
+    appendNumber() {
+ 
+    }
+
+    chooseOperation() {
+
+    }
+}
