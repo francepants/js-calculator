f36ae78076bdfe998312311d06db6bd473a77bd6
Franceline Casanas <Franceline90@gmail.com>
2021-01-05 09:55:42 -0600

    added a random color generator button

--- script.js
+++ script.js
@@ -186,3 +186,13 @@ deleteButton.addEventListener('click', button => {
     calculator.updateDisplay()
 })
 
+// random color 
+
+const setBg = () => {
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    color.innerHTML = "#" + randomColor;
+  }
+  
+  genNew.addEventListener("click", setBg);
+  setBg();
